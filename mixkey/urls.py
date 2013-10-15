e39930b7e351de1d95aa40e411095f202930824a
crosalot <crosalot@gmail.com>
2013-10-15 18:42:18 +0530

    add hell

--- mixkey/urls.py
+++ mixkey/urls.py
@@ -1,5 +1,6 @@
-from django.conf.urls import patterns, include, url
+from django.conf import settings
 
+from django.conf.urls import patterns, include, url
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
@@ -14,4 +15,14 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    
+    url(r'^$', 'domain.views.home', name='home'),
 )
+
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    )

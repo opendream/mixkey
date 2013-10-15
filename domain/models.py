d705c5b159d5b44321900af4c70aef62d736561b
crosalot <crosalot@gmail.com>
2013-10-15 21:23:55 +0530

    add c

--- domain/models.py
+++ domain/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    
+    code        = models.CharField(max_length=255, unique=True) # Required
+    name        = models.CharField(null=True, max_length=255)
+    description = models.TextField(blank=True)
+    
+    created     = models.DateTimeField(auto_now_add=True)
+    
+    
+class Sensor(models.Model):
+    
+    project     = models.ForeignKey(Project) # Required
+    
+    code        = models.CharField(max_length=255, unique=True) # Required
+    formula     = models.CharField(null=True, max_length=255)
+    lat         = models.FloatField(null=True)
+    lng         = models.FloatField(null=True)
+               
+    name        = models.CharField(null=True, max_length=255)
+    description = models.TextField(blank=True)
+    
+    created     = models.DateTimeField(auto_now_add=True)
+     
+
+class Data(models.Model):
+    
+    sensor      = models.ForeignKey(Sensor)
+    utrasonic   = models.IntegerField()          # Required
+    temperature = models.FloatField(null=True)
+    humidity    = models.IntegerField(null=True)
+    raingauge   = models.FloatField(null=True)
+    battery     = models.FloatField(null=True)
+    
+    created     = models.DateTimeField(auto_now_add=True)
--- domain/views.py
+++ domain/views.py
@@ -1,6 +1,54 @@
 # Create your views here.
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseNotFound
+
+from domain.models import Project, Sensor, Data
 
 def home(request):
     return HttpResponse('Hello Mixkey')
+    
+def data_create(request):
+    
+    # Prepare data
+    
+    project = request.GET.get('P') or request.GET.get('p') or 'unknow'
+    project, created = Project.objects.get_or_create(code=project)
+    
+    sensor = request.GET.get('M') or request.GET.get('m') or request.GET.get('S') or request.GET.get('s')
+    if not sensor:
+        return HttpResponseNotFound('Error: Sensor must be required (M or S)')
+        
+    try:
+        sensor = Sensor.objects.get(code=sensor)
+    except:
+        sensor = Sensor.objects.create(project=project, code=sensor)
+        
+    utrasonic = request.GET.get('U') or request.GET.get('u')
+    if not utrasonic:
+        return HttpResponseNotFound('Error: Utrasonic must be required (U)')
+        
+    utrasonic = int(utrasonic)
+    
+    # Normal case
+    
+    temperature = request.GET.get('T') or request.GET.get('t') or None
+    if temperature:
+        temperature = float(temperature)
+        
+    humidity = request.GET.get('H') or request.GET.get('h') or None
+    if humidity:
+        humidity = int(humidity)
+    
+    raingauge = request.GET.get('R') or request.GET.get('r') or None    
+    if raingauge:
+        raingauge = float(raingauge)
+    
+    battery = request.GET.get('B') or request.GET.get('b') or None
+    if battery:
+        battery = float(battery)
+    
+    data = Data.objects.create(sensor = sensor, utrasonic = utrasonic, temperature = temperature, humidity = humidity, raingauge = raingauge, battery = battery)
+    
+    
+    return HttpResponse('Data store completed: project: %s, sensor: %s, utrasonic: %s, temperature: %s, humidity: %s, raingauge: %s, battery: %s' % (project.code, sensor.code, utrasonic, temperature, humidity, raingauge, battery))
+    

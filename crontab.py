79e7ea5784676490fef71060b59ab4e5d7690a84
crosalot <crosalot@gmail.com>
2013-10-21 18:58:06 +0700

    add crontab

--- crontab.py
+++ crontab.py
@@ -1,9 +1,15 @@
+#! /usr/bin/env python
 
-from django.core.management import setup_environ
+import sys
+import os
 
-from mixkey import settings
 from domain.tasks import send_daily
 
-setup_environ(settings)
+def setup_environment():
+    pathname = os.path.dirname(sys.argv[0])
+    sys.path.append(os.path.abspath(pathname))
+    sys.path.append(os.path.normpath(os.path.join(os.path.abspath(pathname), '../')))
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+    
 
 send_daily()

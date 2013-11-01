5d481ef1d9ff8c21e34d86bac48564bb7c9b62b9
crosalot <crosalot@gmail.com>
2013-11-01 21:51:16 +0700

    fix code 2

--- domain/models.py
+++ domain/models.py
@@ -109,7 +109,7 @@ class Sensor(models.Model):
         except Data.DoesNotExist:
             pass
         
-        if self.level_red and water_level >= self.level_red:
+        if False and self.level_red and water_level >= self.level_red:
             return 'RED'
         elif self.level_yellow and water_level >= self.level_yellow:
             return 'YELLOW'

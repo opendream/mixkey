32431a43d80f3aac9c91ab6c046d257857ba4f4d
crosalot <crosalot@gmail.com>
2013-11-06 22:52:10 +0700

    add graph daily2

---
+++ mixkey/cron_cache.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+
+import sys
+import os
+
+
+def setup_environment():
+    pathname = os.path.dirname(sys.argv[0])
+    sys.path.append(os.path.abspath(pathname))
+    sys.path.append(os.path.normpath(os.path.join(os.path.abspath(pathname), '../')))
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+    
+setup_environment()
+
+from domain.tasks import build_cache
+build_cache()
+
+print 'run cron cache complete'
